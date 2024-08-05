cb17590e751de538c51ee34f8ea49a4b6aba3762
pianosuki <pianosuki@protonmail.com>
2024-08-04 22:45:25 -0700

    Return 400 if requested map is already ranked

--- api/v1/requests/__init__.py
+++ api/v1/requests/__init__.py
@@ -1,7 +1,7 @@
 from flask import abort
 
 from api import v1 as api
-from app import cr
+from app import cr, oac
 from app.schemas import requests_schema, request_schema
 
 
@@ -20,6 +20,11 @@ def post(body: dict):
 
     if cr.get_request(beatmapset_id=beatmapset_id):
         abort(409, f"The request with beatmapset ID '{beatmapset_id}' already exists")
+
+    beatmapset = oac.get_beatmapset(beatmapset_id)
+
+    if beatmapset["status"] in ("ranked", "approved", "qualified", "loved"):
+        abort(400, f"The beatmapset is already {beatmapset["status"]} on osu!")
 
     api.beatmapsets.post({"beatmapset_id": beatmapset_id})
 
