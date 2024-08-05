abaeaad8e91d3a8ca6698874d7f7a30054765fb7
pianosuki <pianosuki@protonmail.com>
2024-08-04 23:58:37 -0700

    Fix responses to include appropriate error information

--- api/v1/requests/__init__.py
+++ api/v1/requests/__init__.py
@@ -1,4 +1,4 @@
-from flask import abort
+from flask import abort, jsonify
 
 from api import v1 as api
 from app import cr, oac
@@ -14,18 +14,20 @@ def post(body: dict):
     errors = request_schema.validate(body)
 
     if errors:
-        abort(400, "Invalid input data")
+        return jsonify({"error_type": "validation_error", "message": "Invalid input data"}), 400
 
     beatmapset_id = body["beatmapset_id"]
 
     if cr.get_request(beatmapset_id=beatmapset_id):
-        abort(409, f"The request with beatmapset ID '{beatmapset_id}' already exists")
+        return jsonify({"error_type": "already_requested", "message": f"The request with beatmapset ID '{beatmapset_id}' already exists"}), 409
 
     beatmapset = oac.get_beatmapset(beatmapset_id)
 
     if beatmapset["status"] in ("ranked", "approved", "qualified", "loved"):
-        abort(400, f"The beatmapset is already {beatmapset["status"]} on osu!")
+        return jsonify({"error_type": "already_ranked", "message": f"The beatmapset is already {beatmapset['status']} on osu!"}), 400
 
     api.beatmapsets.post({"beatmapset_id": beatmapset_id})
 
     cr.add_request(body)
+
+    return jsonify({"message": "Request submitted successfully!"}), 201
