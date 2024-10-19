5fe442b063e2f8cb11e92e6be5a5a0bb0f0d7950
pianosuki <pianosuki@protonmail.com>
2024-10-18 20:07:22 -0700

    Reorganize API & add API utils

---
+++ api/utils.py
@@ -0,0 +1,10 @@
+def pop_auth_info(kwargs: dict) -> dict:
+    return {key: kwargs.pop(key) for key in ("user", "token_info") if key in kwargs}
+
+
+def prime_query_kwargs(kwargs: dict) -> dict:
+    for key, value in list(kwargs.items()):
+        if key in ("limit", "offset", "reversed"):
+            kwargs["_" + key] = kwargs.pop(key)
+
+    return pop_auth_info(kwargs)
